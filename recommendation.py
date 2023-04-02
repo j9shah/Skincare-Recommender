bece3ee7b3ac986e9f6ea3726b20f890e2e41df8
kerfuffle09 <127909398+kerfuffle09@users.noreply.github.com>
2023-04-02 10:35:48 -0400

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -5,18 +5,27 @@ class AbstractRecommender(network.Network):
     """Abstract Recommender object that will recommend items depending on the preference of the user."""
     def __init__(self):
         super().__init__()
+        self.brand_list = brand_list #need to fix this
+        self.skin_type_list = skin_type_list #need to fix this
+        self.price_range_list = price_range_list #need to fix this
 
     def find_paths(self):
         """Returns a list of paths from one Product to Another"""
+        recommended_products = self.recommend_products(brand_list, skin_type_list, price_range_list)
 
-def similar_items():
-    """Returns a list of items with similar reviews and skin type and price in the order of how similar they are
+        # Return these products
+        return recommended_products
 
-    First, it looks for most similar in terms of skin type
-    If there are ties, put the one with a lower price first
+    def recommend_products(self, brand_list, skin_type_list, price_range_list):
+        """Returns a list of products that match the given brand, skin type, and price range, sorted by ratings"""
+        brand_products = set(brand_list)
+        skin_type_products = set(skin_type_list)
+        price_range_products = set(price_range_list)
 
-    If there is a tie again with the prices, go in alphabetical order of the name of the product.
+        # Get the set of products that appear in all 3 lists
+        recommended_products = brand_products.intersection(skin_type_products, price_range_products)
 
-    Implementation:
+        # Sort the recommended products by their ratings
+        sorted_products = sorted(recommended_products, key=lambda x: float(x.split(",")[5]), reverse=True)
 
-    """
+        return sorted_products
