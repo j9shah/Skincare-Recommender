0fcc27c5d677b043df5cbb9e828568c98e9a02e9
kerfuffle09 <127909398+kerfuffle09@users.noreply.github.com>
2023-04-02 19:55:17 -0400

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -14,18 +14,33 @@ class AbstractRecommender(network.Network):
         self.price_range_dict = price_range_dict
 
     def recommend_products(self):
-        """Return a set of products that match the given product type, skin type, brand, and price range, sorted by
+        """Return a list of products that match the given product type, skin type, brand, and price range, sorted by
         ratings
         """
-        product_set = set(tuple(p) for p in self.product_dict.values()) & set(
-            tuple(s) for s in self.skin_type_dict.values()) & set(tuple(b) for b in self.brand_dict.values()) & set(
-            tuple(p) for p in self.price_range_dict.values())
+        # Check that all dictionaries are non-empty
+        if not all((self.product_dict, self.skin_type_dict, self.brand_dict, self.price_range_dict)):
+            return []
 
-        unique_products = []
-        for product in product_set:
-            if product not in unique_products:
-                unique_products.append(product)
+        # Get the lists of products from the dictionaries
+        product_type = self.product_dict.values()
+        product_type1 = list(product_type)[0]
 
-        sorted_products = sorted(unique_products, key=lambda x: x[5], reverse=True)
+        skin_type = self.skin_type_dict.values()
+        skin_type1 = list(skin_type)[0]
 
-        return sorted_products
+        brand = self.brand_dict.values()
+        brand1 = list(brand)[0]
+
+        price_range = set(self.price_range_dict.keys())
+        price_range1 = list(price_range)[0]
+
+        # Get the intersection of the four lists
+        recommended_products = set(
+            set(product_type1) & set(skin_type1) & set(brand1) & set(price_range1))
+
+        recommended_products_list = list(recommended_products)
+
+        # Sort the list by rating, highest to lowest
+        recommended_products_list.sort(key=lambda x: x['rating'], reverse=True)
+
+        return recommended_products
