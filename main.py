ad0fd20116b6c6b787cceb96334b54c47fba3d97
kerfuffle09 <127909398+kerfuffle09@users.noreply.github.com>
2023-04-03 01:54:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 """
+<<<<<<< Updated upstream
 CSC111 Course Project: SkinGenius: Your Personalized Skincare Matchmaker
 This file is Copyright (c) 2023 Vivian Feng, Cailyn Kim, Jainam Shah, and Jennifer Tan.
+<<<<<<< HEAD
 
 This is the main file of the function that initiates and runs the complete simulation.
+=======
+=======
+CSC111 Course Project: Skincare Recommender
+Vivian Feng, Cailyn Kim, Jainam Shah, Jennifer Tan
+>>>>>>> a63a806151ea0b93cac14cc041ffc418083113f4
 """
 from intialize import read_csv
 import simulation
 
-def runnner() -> None:
-    """ Calls for dataset initialization """
+
+def runner() -> None:
+    """
+    Runs all methods needed in order to return an output
+    """
+    # Loading in csv files
     network = read_csv()
 
-if __name__ == '__main__':
-    # Loading in csv files
     # Getting input from user
     product = simulation.get_category_input()
     skin_type = simulation.get_skin_type_input()
     budget = simulation.get_budget_input()
     brand = simulation.get_brand_input()
 
-
     # performing computations on the data
+    network.filter(budget, product, skin_type, brand)
+
+
+if __name__ == '__main__':
+    runner()
